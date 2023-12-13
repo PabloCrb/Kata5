58edd7f7ceaed7428a2d3490c347463bce6bc6bf
PabloCrb <zerytv75@gmail.com>
2023-12-13 19:25:35 +0000

    Project finished and tested

---
+++ src/main/java/software/ulpgc/PrimesCommand.java
@@ -0,0 +1,67 @@
+package software.ulpgc;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Integer.parseInt;
+import static java.util.stream.Collectors.*;
+
+public class PrimesCommand implements Command {
+
+    @Override
+    public Output execute(Input input) {
+        try {
+            return outputOf(primesBetween(input.get("lowerBound"), input.get("upperBound")));
+        } catch (Exception e) {
+            return outputException();
+        }
+    }
+
+    private Output outputException() {
+        return new Output() {
+            @Override
+            public String result() {
+                return null;
+            }
+
+            @Override
+            public int response() {
+                return 403;
+            }
+        };
+    }
+
+    private Output outputOf(String result) {
+        return new Output() {
+            @Override
+            public String result() {
+                return result;
+            }
+
+            @Override
+            public int response() {
+                return 200;
+            }
+        };
+    }
+
+    private String primesBetween(String lowerBound, String upperBound) {
+        return format(
+                IntStream.rangeClosed(parseInt(lowerBound), parseInt(upperBound)).filter(this::isPrime).boxed().collect(toList())
+        );
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+
+    private String format(List<Integer> primes) {
+        return primes.stream().map(String::valueOf).
+                collect(Collectors.
+                joining(", "));
+    }
+}
